721643b3d1dfad688e05660c5f169e3874711112
Barun Panthi Sharma <barunpanthisharma11@gmail.com>
2025-06-30 08:21:00 +0545

    Initial commit

---
+++ src/main/java/com/example/CollectionProject/CollectionProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.CollectionProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CollectionProject/CollectionProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CollectionProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
